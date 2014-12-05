84077a3886f57a7bfa9ed26dad0a2a3f06a2902e
shankar 1593 <shankar.venkatraman15@gmail.com>
2014-12-05 21:03:34 +0530

    Programming Assignment 2
    With the changes

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,26 @@
 ## functions do
 
 ## Write a short comment describing this function
+##The first function, `makeCacheMatrix` creates a special "vector", which is
+##really a data frame containing a function to
+
+##1.  set the value of the matrix
+##2.  get the value of the matrix
+##3.  set the value of the inverse
+##4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function(x) 
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function(m) 
+        data.frame(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
